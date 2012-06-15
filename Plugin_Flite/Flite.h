02c6f9c784f2984601cb567f5fd6df47561db365
Chase Brownell <minigeek@gmail.com>
2012-06-15 15:05:19 -0400

    Documentation for Flite.h

--- Plugin_Flite/Flite.h
+++ Plugin_Flite/Flite.h
@@ -13,24 +13,42 @@ private:
 
 public:
 
-   /* Thred constructor */
+   /** Thread constructor */
    Flite();
 
-   /* Thread deconstructor */
+   /** Thread deconstructor */
    ~Flite();
 
-   /* Load models from file */
+   /**
+    * Load models from file 
+    * @param modelNames Array of strings which are locations of .festvox files to be imported relative to the .mdf
+    * @param numModels The number of models to be loaded
+    * @returns True if the correct number of models were successfully loaded, false otherwise
+    **/
    bool load(char **modelNames, int numModels);
 
-   /* Basic synthesize function, synthsizes and plays directly */
-   float synthesize(char *voice_name, char *text);
+   /**
+    * Basic synthesize function, synthsizes and plays directly 
+    * @param voiceName The voice to be used, must have been loaded previously with load()
+    * @param text The text to be synthesized
+    * @return The length in seconds of the synthesized speech
+    **/
+   float synthesize(char *voiceName, char *text);
 
-   /* Extract phonemes from and utterance and return a string with phonemes ans durations */
+   /**
+    * Extract phonemes from and utterance and return a string with phonemes ans durations 
+    * @param lip The string which will be filled with the phoneme sequence of the form: "phoneme,length(ms),phoneme,length..."
+    **/
    void getPhonemeSequence(char *lip);
 
-   /* Synthesize text, save result internally */
-   bool synth_text(char *voice_name, char *text);
+   /**
+    * Synthesize text, save result internally
+    * @param voiceName The voice to be used, must have been loaded previously with load()
+    * @param text The text to be synthesized
+    * @return True if the text was successfully synthesized with the given voice, false otherwise
+    **/
+   bool synth_text(char *voiceName, char *text);
 
-   /* Play the wave file that is saved internally */
+   /** Play the wave file that is saved internally, synth_text must have been called first */
    void play_saved();
 };
