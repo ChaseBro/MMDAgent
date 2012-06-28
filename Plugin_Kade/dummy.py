877caa280927781f09d9ea00534113b825fd1df5
Chase Brownell <minigeek@gmail.com>
2012-06-28 15:11:44 -0400

    Updates dummy.py
    dummy.py now recognizes simple sentences of the form:
    "What is/are [person]'s [info]?"
    and generates a correct json object which it prints to stdout.

--- Plugin_Kade/dummy.py
+++ Plugin_Kade/dummy.py
@@ -1,14 +1,26 @@
 import json
 import uuid
+import re
+
+print 'Usage: input a simple sentence of the from:'
+print '"What is/are [Person]\'s [info]?"'
+print
 
 while True:
    s = raw_input()
    if s == "quit":
       break
 
-   uid = uuid.uuid1()
+   m = re.match('[wW]hat (is|are) (\w+ ?\w*)\'s ([\w\s]+)\??', s)
+   if m is not None:
+      plural = (False, True)[m.group(1) == "are"]
+      print "Plural:", plural, ", Person:", m.group(2), ", Property:", m.group(3), "\n"
 
-   qObject = json.dumps({'uuid': uid.int, 'qid': 0, 'type': 'question', 'plainText': s, 'context': {}})
+      uid = uuid.uuid1()
 
-   print qObject
+      qObject = json.dumps({'uuid': uid.int, 'qid': 0, 'type': 'question', 'plainText': s, 'query': [['text',m.group(2)],['type','professor'],['traverse',''],['type',m.group(3)]], 'context': {}, 'plural': plural })
+
+      print qObject
+   else:
+      print "Invalid input."
 
