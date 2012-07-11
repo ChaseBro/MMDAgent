d91cb158c5c24e767af46f3a5ab1bdbd226aa018
Chase Brownell <minigeek@gmail.com>
2012-07-11 13:46:48 -0400

    re-write kade.py, will still change significantly

--- Release/AppData/Kade/kade.py
+++ Release/AppData/Kade/kade.py
@@ -1,18 +1,35 @@
 #Parse Tester
+import json;
 
-
-
-def reply(inParse):
-    parse = str(inParse)
-    if parse=="no parse found":
+def procParse(inParse):
+    print "I see you have a question..."
+    inParse = str(inParse)
+    if inParse=="no parse found":
         return "Sorry, I don't understand what you said."
-    elif 'WhereQuestion' in parse:
-        return "Sorry, I don't know where that is."
-    elif 'WhoQuestion' in parse:
-        return "Sorry, I don't know who that is."
-    elif 'WhatQuestion' in parse:
-        return "Sorry, I don't know the answer to that."
+    parse = json.loads(inParse)
+    if 'Query' in parse.keys():
+        query = parse['Query']
+        if 'extracts' in query.keys():
+            for extract in query['extracts']:
+                if 'InfoQuery' in extract.keys():
+                    infoQuery = extract['InfoQuery']
+                    info = infoQuery['Info'][0]
+                    node = extractLeaves(infoQuery['SpecNode'])
+                    response = 'I think you are looking for info about ' + node + '\'s ' + info + "."
+
     else:
-        return "Sorry, I don't know how to help you with that."
+        response = "Sorry, I don't know how to help you with that."
+
+    return response
+
+def extractLeaves(node):
+    if isinstance(node, list):
+        print ' '.join(node)
+        return ' '.join(node)
+
+    if node[node.keys()[0]] is not None:
+        return extractLeaves(node[node.keys()[0]])
+    else:
+        return node.keys()[0]
 
 #print parse("[WhereQuestion] ( WHERE [Linking] ( IS ) [LocationQuery] ( [Person] ( [Professor] ( NYBERG ) ) 'S [Location] ( [_Office] ( OFFICE ) ) ) ) ")
