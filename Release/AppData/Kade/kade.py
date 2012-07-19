02978b6c606c81177f430188209a95c086ea7568
Chase Brownell <minigeek@gmail.com>
2012-07-19 10:37:15 -0400

    Update Kade.py and testset (has correct url)

--- Release/AppData/Kade/kade.py
+++ Release/AppData/Kade/kade.py
@@ -16,8 +16,11 @@ class JsonServer:
         uid = uuid.uuid1()
         jsonDump = json.dumps({'uuid': uid.int, 'qid': 0, 'type': 'question', 'plainText': plainText, 'query': jsonQuery, 'context': {}, 'plural': False })
         print jsonDump
-        result = self.proxy.parse({'uuid': uid.int, 'qid': 0, 'type': 'question', 'plainText': plainText, 'query': jsonQuery, 'context': {}, 'plural': False })
-        print result
+        try:
+            result = self.proxy.parse({'uuid': uid.int, 'qid': 0, 'type': 'question', 'plainText': plainText, 'query': jsonQuery, 'context': {}, 'plural': False })
+            print result
+        except:
+            result = None
         return result
 
 myInfo = {'name' : 'Fergus', 'email': 'fergus at c.m.u dot e.d.u', 'department': 'Language Technologies', 'firstName': 'Fergus', 'lastName': 'Carnegie'}
@@ -29,7 +32,7 @@ def procParse(plainText, inParse):
         return "I'm sorry, I'm a little hard of hearing, can ya repeat that?"
     parse = json.loads(inParse)
 
-    server = JsonServer('http://128.237.232.50:8080')
+    server = JsonServer('http://peace.isri.cs.cmu.edu:9080/constellation/')
 
     if 'Query' in parse.keys():
         query = parse['Query']
@@ -38,7 +41,10 @@ def procParse(plainText, inParse):
             for extract in query['extracts']:
                 if 'InfoQuery' in extract.keys():
                     infoQuery = extract['InfoQuery']
-                    infoNode = infoQuery['Info']
+                    if 'Info' in infoQuery.keys():
+                        infoNode = extractLeaves(infoQuery['Info'])
+                    else:
+                        infoNode = extractLeaves(infoQuery['InfoVerb'])
                     specNode = infoQuery['SpecNode']
                     if extractLeaves(specNode) == 'you':
                         if infoNode == 'phone':
@@ -46,33 +52,36 @@ def procParse(plainText, inParse):
                         else:
                             response = 'My ' + (infoNode, 'last name')[infoNode == 'lastName'] + ' is ' + myInfo[infoNode] + '.'
                     else:
-                        jsonQuery = jsonSpecNode(infoQuery['SpecNode']) , ('Traverse', None), ('Type', infoNode)
+                        jsonQuery = jsonSpecNode(infoQuery['SpecNode']) + (('Traverse', None), ('Type', infoNode))
                         answer = server.callNell(plainText, jsonQuery)
                         if answer is not None and answer['plainText'] is not None:
                             response = extractLeaves(specNode) + '\'s ' + infoNode + ' is ' + answer['plainText']
-                        response = 'I think you are looking for info about ' + extractLeaves(specNode) + '\'s ' + infoNode + "."
+                        else:
+                            response = 'I\'m sorry, I don\'t have any info about ' + extractLeaves(specNode) + '\'s ' + infoNode + "."
 
                 elif 'NodeQuery' in extract.keys():
                     nodeQuery = extract['NodeQuery']
-                    genNode = nodeQuery['GenNode']
-                    genNodeType = getNodeType(nodeQuery['GenNode'])
+                    genNode = extractLeaves(nodeQuery['GenNode'])
                     specNode = nodeQuery['SpecNode']
                     relVerb = nodeQuery['RelVerb']
 
                     jsonQuery = jsonSpecNode(specNode)
 
-                    if 'AllLinking' in nodeQuery.keys():
-                        response = 'I think you want to know what ' + genNodeType + ' ' + extractLeaves(nodeQuery['AllLinking']) + ' ' + extractLeaves(specNode) + ' ' + extractLeaves(relVerb) + '.'
-                        jsonQuery += ', ["Traverse", "' + extractLeaves(relVerb) + '"]'
+                    if 'AllLinking' in nodeQuery.keys() and ( 'Prep' not in nodeQuery.keys() or nodeQuery['Prep'][0] != 'BY' ):
+                        response = 'I think you want to know what ' + genNode + ' ' + extractLeaves(nodeQuery['AllLinking']) + ' ' + extractLeaves(specNode) + ' ' + extractLeaves(relVerb) + '.'
+                        jsonQuery += (('Traverse', extractLeaves(relVerb)),)
                     else:
-                        response = 'I think you want to know what ' + genNodeType + ' ' + extractLeaves(relVerb) + ' ' + extractLeaves(specNode) + '.'
-                        jsonQuery += ', ["TraverseIn", "' + extractLeaves(relVerb) + '"]'
+                        response = 'I think you want to know what ' + genNode + ' ' + extractLeaves(relVerb) + ' ' + extractLeaves(specNode) + '.'
+                        jsonQuery += (('TraverseIn', extractLeaves(relVerb)),)
 
-                    if genNodeType is not None:
-                        jsonQuery += ', ["Type", "' + genNodeType + '"]'
+                    jsonQuery += getNodeType(nodeQuery['GenNode'])
+
+                    answer = server.callNell(plainText, jsonQuery)
+                    if answer is not None and answer['plainText'] is not None:
+                        response = genNodeType + '\'s ' + infoNode + ' is ' + answer['plainText']
 
                 elif 'LocationQuery' in extract.keys():
-                    locQuery = exract['LocationQuery']
+                    locQuery = extract['LocationQuery']
                     if 'SpecLocation' in locQuery:
                         jsonQuery = '["Text", "' + extractLeaves(locQuery['SpecLocation']) + '"]'
                     elif 'GenLocation' in locQuery:
@@ -83,10 +92,11 @@ def procParse(plainText, inParse):
                     specNode = extract['SpecNode']
                     response = 'I think you want to know something about a location.'
 
-        if jsonQuery is not None:
-            print jsonQuery
     else:
-        response = "Sorry, I don't know how to help you with that."
+        answer = server.callNell(plainText, None)
+        if answer is not None and answer['plainText'] is not None:
+            response = 'I think the answer you\'re looking for is ' + answer['plainText']
+        response = 'Sorry, I don\'t know how to help you with that.'
 
     return response
 
@@ -94,18 +104,20 @@ def extractLeaves(node):
     if isinstance(node, list):
         return ' '.join(node)
 
-    if node[node.keys()[0]] is not None:
-        return extractLeaves(node[node.keys()[0]])
-    else:
-        return node.keys()[0]
+    if isinstance(node, dict):
+        if node[node.keys()[0]] is not None:
+            return extractLeaves(node[node.keys()[0]])
+        else:
+            return node.keys()[0]
+    return node
 
 # Extract the json query from a SpecNode (Specific Node)
 def jsonSpecNode(specNode):
     if 'SpecPerson' in specNode:
-        jsonQuery = ('Type', 'Person')
-        jsonQuery += ('Text', extractLeaves(specNode))
+        jsonQuery = (('Type', 'person'),)
+        jsonQuery += (('Text', extractLeaves(specNode)),)
     elif 'SpecLocation' in specNode:
-        jsonQuery = ('Type', 'Location')
+        jsonQuery = (('Type', 'location'),)
         jsonQuery += ('Text', extractLeaves(specNode))
     else:
         jsonQuery = ('Text', extractLeaves(specNode))
@@ -115,12 +127,12 @@ def jsonSpecNode(specNode):
 # Extracts the node type of a node (such as 'person', 'location', etc.)
 def getNodeType(node):
     label = node.keys()[0]
+    ret = ()
     if label == 'SpecPerson' or label == 'GenPerson':
-        return 'person'
+        ret += (('Type', 'person'),)
     elif label == 'SpecLocation' or label == 'GenLocation':
-        return 'location'
-    else:
-        return None
+        ret += (('Type', 'location'),)
+    return ret
 
 #print parse("[WhereQuestion] ( WHERE [Linking] ( IS ) [LocationQuery] ( [Person] ( [Professor] ( NYBERG ) ) 'S [Location] ( [_Office] ( OFFICE ) ) ) ) ")
 if __name__ == '__main__':
@@ -130,8 +142,6 @@ if __name__ == '__main__':
     if len(args) < 5:
         print 'Usage: python kade.py [path to parse_text] -dir [path to grammar dir] -grammar [grammar file name]'
         quit()
-    print args
-    print
 
     print 'Usage: input a simple sentence of the from:'
     print '"What is/are [Person]\'s [info]?"'
@@ -144,9 +154,9 @@ if __name__ == '__main__':
 
         if s is not '':
             out, err = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE).communicate(s)
-            print 'err:',err
-            print 'out:',out
+            print out
             print procParse(s, out)
+            print
 
         else:
             print "Invalid input."
